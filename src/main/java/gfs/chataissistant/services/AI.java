8e67eae88e9ff148c0bc7014e07324e0515bbb44
IsThisALis <gamemodebronze2@gmail.com>
2026-06-24 15:53:26 +0500

    feat(AI) - add LM Studio support

--- src/main/java/gfs/chataissistant/services/AI.java
+++ src/main/java/gfs/chataissistant/services/AI.java
@@ -14,21 +14,20 @@ public class AI {
   private static HttpRequest request;
   private static HttpResponse<String> response;
 
+//  public static String apiUrl;
   private static String json;
+
   private static String model;
   private static String apiKey; 
-  public static String apiUrl;
 
 
   public static void init() {
-    model = IO.loadTextFile("configs/model.txt");
-    apiKey = IO.loadTextFile("configs/apiKey.txt");
-    apiUrl = "https://openrouter.ai/api/v1/chat/completions";
+    model = Config.getConfig().model;
+    apiKey = Config.getConfig().apiKey;
   }
 
 
   public static String ask(String message) {
- 
     try {
       json = JSON.receiveMessage(message, model);
     } catch (Exception e) {
@@ -36,21 +35,35 @@ public class AI {
       return "none";
     }
 
-    request = HttpRequest.newBuilder()
-      .uri(URI.create(apiUrl))
-      .header("Authorization", "Bearer " + apiKey)
-      .header("Content-Type", "application/json")
-      .header("HTTP-Referer", "http://localhost")
-      .header("X-Title", "VTuberBot")
-      .POST(HttpRequest.BodyPublishers.ofString(json))
-      .build();
+
+    if (Config.getConfig().apiProvider.toLowerCase().equals("lms")) {
+      request = HttpRequest.newBuilder()
+        .version(HttpClient.Version.HTTP_1_1)
+        .uri(URI.create("http://127.0.0.1:1234/v1/chat/completions"))
+        .header("Authorization", "Bearer "+apiKey)
+        .header("Content-Type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(json))
+        .build();
+    }
+
+
+    if (Config.getConfig().apiProvider.toLowerCase().equals("openrouter")) {
+      request = HttpRequest.newBuilder()
+        .uri(URI.create("https://openrouter.ai/api/v1/chat/completions"))
+        .header("Authorization", "Bearer " + apiKey)
+        .header("Content-Type", "application/json")
+        .header("HTTP-Referer", "http://localhost")
+        .header("X-Title", "ChatAIssistant")
+        .POST(HttpRequest.BodyPublishers.ofString(json))
+        .build();
+    }
 
     try {
-      System.out.println("Request sent to "+model);
+      System.out.println("Request sent to: "+model);
       response = http.send(request, HttpResponse.BodyHandlers.ofString());
 
       if (response.statusCode() != 200) throw new RuntimeException("API error: " + response.statusCode() + ": " + response.body());
-      if (response != null) System.out.println("Got response from "+model+" response: "+response);
+      if (response != null) { System.out.println("Got response from "+model+" response: "+response); }
     } catch (Exception e) {
       System.err.println("Error! "+e);
       e.printStackTrace();
